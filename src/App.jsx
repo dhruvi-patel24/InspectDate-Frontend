42ac946a9e2e08c590ac8511e9a4e160769e5f8b
dhruvi-patel24 <patel24dhruvi@gmail.com>
2025-12-12 17:32:54 +0530

    Sign In page related changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.scss'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/home/home';
-// import ProjectListing from './pages/project/projectListing';
-// import BankListing from './pages/bank/bankListing';
+import SignIn from './components/auth/SignIn';
+import DashboardLayout from './components/dashboard/DashboardLayout';
+import Calendar from './components/dashboard/Calendar';
+import Projects from './components/dashboard/Projects';
+import { ROUTES } from './routes/routePaths';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app-container">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={ROUTES.sign_in} element={<SignIn />} />
+
+          {/* Dashboard Routes */}
+          <Route element={<DashboardLayout />}>
+            <Route path={ROUTES.dashboard} element={<div className="p-4"><h1>Welcome to Dashboard</h1></div>} />
+            <Route path={ROUTES.calendar} element={<Calendar />} />
+            <Route path={ROUTES.projects} element={<Projects />} />
+            {/* Add other routes as placeholders for now */}
+            <Route path="*" element={<div>Page Under Construction</div>} />
+          </Route>
+        </Routes>
       </div>
     </BrowserRouter>
   )
