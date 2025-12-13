98fabd751f251f0e9f823c30eade9f7cee924ad4
dhruvi-patel24 <patel24dhruvi@gmail.com>
2025-12-13 12:16:04 +0530

    Moved the project component to the projects folder from dashboard folder

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Home from './components/home/home';
 import SignIn from './components/auth/SignIn';
 import DashboardLayout from './components/dashboard/DashboardLayout';
 import Calendar from './components/dashboard/Calendar';
-import Projects from './components/dashboard/Projects';
+import Projects from './components/projects/Projects';
+import ProjectTransfers from './components/transfers/ProjectTransfers';
 import { ROUTES } from './routes/routePaths';
 
 function App() {
@@ -21,8 +22,9 @@ function App() {
           {/* Dashboard Routes */}
           <Route element={<DashboardLayout />}>
             <Route path={ROUTES.dashboard} element={<div className="p-4"><h1>Welcome to Dashboard</h1></div>} />
-            <Route path={ROUTES.calendar} element={<Calendar />} />
-            <Route path={ROUTES.projects} element={<Projects />} />
+            <Route path={ROUTES.calendar} element={ <Calendar /> } />
+            <Route path={ROUTES.projects} element={ <Projects /> } />
+            <Route path={ROUTES.project_transfers} element={ <ProjectTransfers /> } />
             {/* Add other routes as placeholders for now */}
             <Route path="*" element={<div>Page Under Construction</div>} />
           </Route>
